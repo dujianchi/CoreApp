a42436d57a08fcd3a01291df2b5a3c1eaa01aa0b
du <727809864@qq.com>
2018-11-02 14:12:31 +0800

    优化:~~BaseListXXX的方法统一提出来~~ (有问题，删除，回滚这个方法)

--- core/src/main/java/cn/dujc/core/ui/BaseListFragment.java
+++ core/src/main/java/cn/dujc/core/ui/BaseListFragment.java
@@ -3,117 +3,197 @@ package cn.dujc.core.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
+import cn.dujc.core.R;
 import cn.dujc.core.adapter.BaseQuickAdapter;
 
 /**
  * 基本列表fragment
  * Created by du on 2017/9/27.
  */
-public abstract class BaseListFragment extends BaseFragment implements IBaseList.UI {
+public abstract class BaseListFragment extends BaseFragment {
 
-    private IBaseList mBaseList;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        if (mBaseList == null) mBaseList = new FragmentImpl(this);
-        super.onCreate(savedInstanceState);
-    }
+    private SwipeRefreshLayout mSrlLoader;
+    private RecyclerView mRvList;
+    private BaseQuickAdapter mQuickAdapter;
 
     @Override
     public int getViewId() {
-        return mBaseList.getViewId();
+        return R.layout.base_list_layout;
     }
 
     @Override
     public void initBasic(Bundle savedInstanceState) {
-        mBaseList.initBasic(savedInstanceState);
+        mSrlLoader = (SwipeRefreshLayout) findViewById(R.id.srl_loader);
+        mRvList = (RecyclerView) findViewById(R.id.rv_list);
+
+        //mSrlLoader.setColorSchemeResources(R.color.colorPrimary, R.color.colorAccent);
+        mSrlLoader.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                if (mQuickAdapter != null) {
+                    mQuickAdapter.resetLoadMore();
+                }
+                reload();
+            }
+        });
+
+        mQuickAdapter = initAdapter();
+        final RecyclerView.LayoutManager layoutManager = initLayoutManager();
+
+        mRvList.setLayoutManager(layoutManager);
+        if (mQuickAdapter != null) {
+            mRvList.setAdapter(mQuickAdapter);
+
+            mQuickAdapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
+                @Override
+                public void onLoadMoreRequested() {
+                    loadMore();
+                }
+            }, mRvList);
+
+            mQuickAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+                @Override
+                public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                    BaseListFragment.this.onItemClick(position);
+                }
+            });
+
+            //initAdapter.disableLoadMoreIfNotFullPage();
+            mQuickAdapter.openLoadAnimation();
+        }
+        recyclerViewOtherSetup();
+
+        loadAtFirst();
     }
 
     @Override
-    public void onDestroy_() {
-        mBaseList.onDestroy_();
+    public void onDestroy() {
+        if (mQuickAdapter != null) mQuickAdapter.onRecycled();
+        super.onDestroy();
     }
 
-    @Override
-    public void coordinateRefreshAndAppbar() {
-        mBaseList.coordinateRefreshAndAppbar();
+    /**
+     * 加载第一次数据，默认同reload方法
+     */
+    protected void loadAtFirst() {
+        reload();
     }
 
-    @Override
-    public void loadAtFirst() {
-        mBaseList.loadAtFirst();
+    /**
+     * 加载失败（部分刷新）
+     */
+    protected final void loadFailure() {
+        refreshDone();
+        if (mQuickAdapter != null) {
+            mQuickAdapter.loadMoreFail();
+        }
     }
 
-    @Override
-    public void loadFailure() {
-        mBaseList.loadFailure();
+    /**
+     * 刷新结束
+     */
+    protected final void refreshDone() {
+        if (mSrlLoader != null) {
+            mSrlLoader.setRefreshing(false);
+        }
     }
 
-    @Override
-    public void refreshDone() {
-        mBaseList.refreshDone();
+    /**
+     * 加载结束
+     * @param dataDone 数据加载结束
+     * @param endGone adapter的尾部是否隐藏
+     */
+    protected void loadDone(boolean dataDone, boolean endGone){
+        if (mQuickAdapter != null) {
+            if (dataDone) {
+                mQuickAdapter.loadMoreEnd(endGone);
+            } else {
+                mQuickAdapter.loadMoreComplete();
+            }
+        }
     }
 
-    @Override
-    public void loadDone(boolean dataDone, boolean endGone) {
-        mBaseList.loadDone(dataDone, endGone);
+    /**
+     * 全部刷新
+     */
+    protected final void notifyDataSetChanged(boolean done) {
+        notifyDataSetChanged(done, true);
     }
 
-    @Override
-    public void notifyDataSetChanged(boolean done) {
-        mBaseList.notifyDataSetChanged(done);
-    }
-
-    @Override
-    public void notifyDataSetChanged(boolean dataDone, boolean endGone) {
-        mBaseList.notifyDataSetChanged(dataDone, endGone);
+    /**
+     * 全部刷新
+     */
+    protected final void notifyDataSetChanged(boolean dataDone, boolean endGone) {
+        refreshDone();
+        if (mQuickAdapter != null) {
+            loadDone(dataDone, endGone);
+            mQuickAdapter.notifyDataSetChanged();
+        }
     }
 
     @Nullable
-    @Override
-    public <T> T getItem(int position) {
-        return mBaseList.getItem(position);
+    protected final <T> T getItem(int position) {
+        return mQuickAdapter != null ? (T) mQuickAdapter.getItem(position) : null;
     }
 
-    @Override
-    public void recyclerViewOtherSetup() {
-        mBaseList.recyclerViewOtherSetup();
+    /**
+     * 关于recyclerView的一些其他设置
+     */
+    protected void recyclerViewOtherSetup() {
+        if (mRvList != null) {
+            DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
+            mRvList.addItemDecoration(dividerItemDecoration);
+        }
+    }
+
+    /**
+     * 默认的RecyclerView.LayoutManager是LinearLayoutManager
+     *
+     * @return LinearLayoutManager
+     */
+    @Nullable
+    protected RecyclerView.LayoutManager initLayoutManager() {
+        return new LinearLayoutManager(mActivity);
     }
 
     @Nullable
-    @Override
-    public RecyclerView.LayoutManager initLayoutManager() {
-        return mBaseList.initLayoutManager();
-    }
-
-    @Nullable
-    @Override
-    public BaseQuickAdapter getAdapter() {
-        return mBaseList.getAdapter();
-    }
-
-    @Nullable
-    @Override
     public RecyclerView getRecyclerView() {
-        return mBaseList.getRecyclerView();
+        return mRvList;
     }
 
     @Nullable
-    @Override
-    public SwipeRefreshLayout getSwipeRefreshLayout() {
-        return mBaseList.getSwipeRefreshLayout();
+    public BaseQuickAdapter getAdapter() {
+        return mQuickAdapter;
     }
 
-    @Override
-    public void showRefreshing() {
-        mBaseList.showRefreshing();
+    @Nullable
+    protected final SwipeRefreshLayout getSwipeRefreshLayout() {
+        return mSrlLoader;
     }
 
-    @Override
-    public void refreshEnable(boolean enable) {
-        mBaseList.refreshEnable(enable);
+    protected final void showRefreshing() {
+        if (mSrlLoader != null) {
+            mSrlLoader.setRefreshing(true);
+        }
     }
 
+    protected final void refreshEnable(boolean enable) {
+        if (mSrlLoader != null) {
+            mSrlLoader.setEnabled(enable);
+        }
+    }
+
+    @Nullable
+    protected abstract BaseQuickAdapter initAdapter();
+
+    protected abstract void onItemClick(int position);
+
+    protected abstract void loadMore();
+
+    protected abstract void reload();
 }
