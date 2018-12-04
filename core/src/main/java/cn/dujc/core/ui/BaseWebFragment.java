a4e8351cbe2b197504688365c2299f1ad87a1da1
do <727809864@qq.com>
2018-12-04 23:10:37 +0800

    update BaseWebViewFragment

--- core/src/main/java/cn/dujc/core/ui/BaseWebFragment.java
+++ core/src/main/java/cn/dujc/core/ui/BaseWebFragment.java
@@ -43,8 +43,7 @@ public class BaseWebFragment extends BaseRefreshableFragment {
     private static final String TITLE_INTENT = "TITLE_INTENT";
     private static final String URL_INTENT = "URL_INTENT";
 
-    private final static String[] DB_NAME_LIST = {"webview.db", "webviewCache.db", "webviewCookiesChromium.db"
-            , "webviewCookiesChromiumPrivate.db"};
+    private final static String[] DB_NAME_LIST = {"webview.db", "webviewCache.db", "webviewCookiesChromium.db", "webviewCookiesChromiumPrivate.db"};
 
     private ProgressBar pb_progressbar;
     private WebView web_simple_view;
@@ -60,9 +59,39 @@ public class BaseWebFragment extends BaseRefreshableFragment {
         }
     }
 
-    @Override
-    public int getViewId() {
-        return R.layout.fragment_base_web;
+    /**
+     * 对webView的DB进行访问，如果有异常，则判断当前进程不能使用WebView
+     *
+     * @param context
+     */
+    public static boolean isWebViewAvailable(final Context context) {
+
+        boolean checkExisted = false;
+
+        boolean bEnable = true;
+        for (String dbName : DB_NAME_LIST) {
+            // 构造文件，判断有无存在
+            File dbFile = new File(context.getFilesDir().getParentFile().getPath() + "/databases/" + "local_" + dbName);
+            if (dbFile.exists()) {
+                if (!isOpenSuccess(context, dbName)) {
+                    bEnable = false;
+                }
+                checkExisted = true;
+            }
+        }
+
+        if (!checkExisted) {
+            final String dumyDb = "dumyDb";
+            // 加强一下
+            bEnable = isOpenSuccess(context, dumyDb);
+            if (!bEnable) {
+                try {
+                    context.deleteDatabase(dumyDb);
+                } catch (Throwable e) {
+                }
+            }
+        }
+        return bEnable;
     }
 
     @Override
@@ -87,86 +116,6 @@ public class BaseWebFragment extends BaseRefreshableFragment {
         init();
     }
 
-    private void init() {
-        if (!TextUtils.isEmpty(mTitle)) {
-            mActivity.setTitle(mTitle);
-        }
-
-        web_simple_view = (WebView) findViewById(R.id.web_simple_view);
-        pb_progressbar = (ProgressBar) findViewById(R.id.pb_progressbar);
-
-        WebSettings settings = web_simple_view.getSettings();
-        settings.setUserAgentString(settings.getUserAgentString() + "" + mActivity.getPackageName());
-
-        settings.setJavaScriptEnabled(true);
-        settings.setJavaScriptCanOpenWindowsAutomatically(true);
-
-        settings.setAllowFileAccess(true);
-
-        settings.setUseWideViewPort(true);
-        settings.setLoadWithOverviewMode(true);
-        settings.setBuiltInZoomControls(true);
-        settings.setDomStorageEnabled(true);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            settings.setDisplayZoomControls(false);
-        }
-
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
-            settings.setRenderPriority(WebSettings.RenderPriority.HIGH);//提高渲染优先级
-            settings.setSavePassword(false);
-            settings.setPluginState(WebSettings.PluginState.ON);
-        }
-
-        pb_progressbar.setMax(100);
-        pb_progressbar.setProgressDrawable(ContextCompat.getDrawable(mActivity, R.drawable.progress_bar_states));
-        pb_progressbar.setProgress(5); //先加载5%，以使用户觉得界面没有卡死
-
-        web_simple_view.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                return super.shouldOverrideUrlLoading(view, url);
-            }
-
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-                pb_progressbar.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                pb_progressbar.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                super.onReceivedError(view, errorCode, description, failingUrl);
-                pb_progressbar.setVisibility(View.GONE);
-            }
-        });
-        web_simple_view.setWebChromeClient(new WebChromeClient() {
-
-            @Override
-            public void onReceivedTitle(WebView view, String title) {
-                super.onReceivedTitle(view, title);
-                if (TextUtils.isEmpty(mTitle)) {
-                    mActivity.setTitle(title);
-                }
-            }
-
-            @Override
-            public void onProgressChanged(WebView view, int newProgress) {
-                if (newProgress >= 5) {
-                    pb_progressbar.setProgress(newProgress);
-                }
-                super.onProgressChanged(view, newProgress);
-            }
-        });
-        loadUrl(mUrl);
-    }
-
     @Override
     public void onPause() {
         super.onPause();
@@ -187,56 +136,6 @@ public class BaseWebFragment extends BaseRefreshableFragment {
     public void onDestroy() {
         web_simple_view.destroy();
         super.onDestroy();
-    }
-
-    /**
-     * 能否处理intent
-     *
-     * @param context
-     * @param intent
-     * @return
-     */
-    public boolean hasIntentHandler(Context context, Intent intent) {
-        PackageManager pm = context.getPackageManager();
-        List<ResolveInfo> list = pm.queryIntentActivities(intent,
-                PackageManager.MATCH_DEFAULT_ONLY);
-        return list.size() > 0;
-    }
-
-    /**
-     * 对webView的DB进行访问，如果有异常，则判断当前进程不能使用WebView
-     *
-     * @param context
-     */
-    public static boolean isWebViewAvailable(final Context context) {
-
-        boolean checkExisted = false;
-
-        boolean bEnable = true;
-        for (String dbName : DB_NAME_LIST) {
-            // 构造文件，判断有无存在
-            File dbFile = new File(context.getFilesDir().getParentFile().getPath()
-                    + "/databases/" + "local_" + dbName);
-            if (dbFile.exists()) {
-                if (!isOpenSuccess(context, dbName)) {
-                    bEnable = false;
-                }
-                checkExisted = true;
-            }
-        }
-
-        if (!checkExisted) {
-            final String dumyDb = "dumyDb";
-            // 加强一下
-            bEnable = isOpenSuccess(context, dumyDb);
-            if (!bEnable) {
-                try {
-                    context.deleteDatabase(dumyDb);
-                } catch (Throwable e) {
-                }
-            }
-        }
-        return bEnable;
     }
 
     public static boolean isOpenSuccess(Context context, String dbName) {
@@ -263,7 +162,128 @@ public class BaseWebFragment extends BaseRefreshableFragment {
         return isOpenSuccess;
     }
 
-    public boolean onBackPressed() {
+    @Override
+    public int getViewId() {
+        return R.layout.layout_base_web;
+    }
+
+    @Override
+    public void onRefresh() {
+        if (web_simple_view != null) web_simple_view.reload();
+        refreshDone();
+    }
+
+    private void init() {
+        if (!TextUtils.isEmpty(mTitle)) {
+            mActivity.setTitle(mTitle);
+        }
+
+        web_simple_view = (WebView) findViewById(R.id.web_simple_view);
+        pb_progressbar = (ProgressBar) findViewById(R.id.pb_progressbar);
+
+        pb_progressbar.setMax(100);
+        pb_progressbar.setProgressDrawable(ContextCompat.getDrawable(mActivity, R.drawable.progress_bar_states));
+        pb_progressbar.setProgress(5); //先加载5%，以使用户觉得界面没有卡死
+
+        initWebViewSettings();
+
+        web_simple_view.setWebViewClient(getWebViewClient());
+        web_simple_view.setWebChromeClient(getWebChromeClient());
+
+        loadAtFirst();
+    }
+
+    protected void initWebViewSettings() {
+        WebSettings settings = web_simple_view.getSettings();
+        settings.setUserAgentString(settings.getUserAgentString() + "" + mActivity.getPackageName());
+
+        settings.setJavaScriptEnabled(true);
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);
+
+        settings.setAllowFileAccess(true);
+
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setBuiltInZoomControls(true);
+        settings.setDomStorageEnabled(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            settings.setDisplayZoomControls(false);
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            settings.setRenderPriority(WebSettings.RenderPriority.HIGH);//提高渲染优先级
+            settings.setSavePassword(false);
+            settings.setPluginState(WebSettings.PluginState.ON);
+        }
+    }
+
+    protected WebViewClient getWebViewClient() {
+        return new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return super.shouldOverrideUrlLoading(view, url);
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                pb_progressbar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                pb_progressbar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                super.onReceivedError(view, errorCode, description, failingUrl);
+                pb_progressbar.setVisibility(View.GONE);
+            }
+        };
+    }
+
+    protected WebChromeClient getWebChromeClient() {
+        return new WebChromeClient() {
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                if (TextUtils.isEmpty(mTitle)) {
+                    mActivity.setTitle(title);
+                }
+            }
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress >= 5) {
+                    pb_progressbar.setProgress(newProgress);
+                }
+                super.onProgressChanged(view, newProgress);
+            }
+        };
+    }
+
+    protected void loadAtFirst() {
+        loadUrl(mUrl);
+    }
+
+    /**
+     * 能否处理intent
+     *
+     * @param context
+     * @param intent
+     * @return
+     */
+    public boolean hasIntentHandler(Context context, Intent intent) {
+        PackageManager pm = context.getPackageManager();
+        List<ResolveInfo> list = pm.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+
+    public final boolean onBackPressed() {
         if (web_simple_view.canGoBack()) {
             web_simple_view.goBack();
             return true;
@@ -271,17 +291,19 @@ public class BaseWebFragment extends BaseRefreshableFragment {
         return false;
     }
 
-    public void loadUrl(String url) {
+    public final void loadUrl(String url) {
+        loadUrl(url, true);
+    }
+
+    public final void loadUrl(String url, boolean clear) {
         LogUtil.d("load url = " + url);
         if (web_simple_view != null) {
-            web_simple_view.clearHistory();
+            if (clear) web_simple_view.clearHistory();
             web_simple_view.loadUrl(mUrl = url);
         }
     }
 
-    @Override
-    public void onRefresh() {
-        if (web_simple_view != null) web_simple_view.reload();
-        refreshDone();
+    public final WebView getWebView() {
+        return web_simple_view;
     }
 }
